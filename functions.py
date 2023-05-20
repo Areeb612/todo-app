026b62090fe8cf06974993a2aa2049c1fdac1d47
Areeb U.R <areeburrehman43@gmail.com>
2023-05-19 22:17:01 -0400

    Add global variable in functions.py

--- functions.py
+++ functions.py
@@ -1,12 +1,15 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+
+def get_todos(FILEPATH):
     """ Read a text file and return the list of to-do items. """
-    with open(filepath, "r") as file_local:
+    with open(FILEPATH, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, FILEPATH):
     """ Write the to-do items list in the text file. """
-    with open(filepath, 'w') as file_local:
+    with open(FILEPATH, 'w') as file_local:
         file_local.writelines(todos_arg)
 
